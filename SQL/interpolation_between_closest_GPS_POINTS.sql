8e5f58f95bd67f0e5cfc3e14d894bc6d460d29f3
juldebar <juldebar@gmail.com>
2018-07-04 15:23:38 +0400

    add / udpate SQL queries to create and extract data in the database

--- SQL/interpolation_between_closest_GPS_POINTS.sql
+++ SQL/interpolation_between_closest_GPS_POINTS.sql
@@ -1,3 +1,8 @@
-SELECT ST_AsEWKT(ST_Line_Interpolate_Point(the_line, 0.20)) FROM (SELECT ST_AsEWKT(ST_MakeLine(wkb_geometry)) AS the_line FROM track_points WHERE (time=('2018-03-31 13:17:09+02')::timestamp OR time=('2018-03-31 13:17:10+02')::timestamp) AND session_id='XXXXXX') AS foo;
+﻿SELECT photos_exif_core_metadata.session_id,photos_exif_core_metadata.session_photo_number,photos_exif_core_metadata.datetimeor, ST_AsEWKT(ST_Line_Interpolate_Point(the_line, 0.50))
+FROM
+photos_exif_core_metadata,
+(SELECT ST_AsEWKT(ST_MakeLine(gps_tracks.the_geom)) AS the_line FROM gps_tracks, photos_exif_core_metadata  WHERE ( gps_tracks.time=(photos_exif_core_metadata.datetimeor::timestamp - interval '13400 second') OR gps_tracks.time=(photos_exif_core_metadata.datetimeor::timestamp) - interval '13401 second') AND gps_tracks.session_id='session_2018_02_25_paddle_Le_Morne') AS foo ;
 
 
+--(gps_tracks.time=(photos_exif_core_metadata.datetimeor::timestamp - interval '13400 second') OR time=(photos_exif_core_metadata.datetimeor::timestamp) - interval '13401 second' )
+--, MIN(gps_tracks.time), MIN(gps_tracks.time)
