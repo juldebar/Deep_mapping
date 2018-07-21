e029f712ee3f22dd5bda43b6a9aac0664859e138
juldebar <juldebar@gmail.com>
2018-07-21 17:42:13 +0400

    improve query to correlate GPS time with camera time

--- SQL/interpolation_between_closest_GPS_POINTS.sql
+++ SQL/interpolation_between_closest_GPS_POINTS.sql
@@ -6,3 +6,20 @@ photos_exif_core_metadata,
 
 --(gps_tracks.time=(photos_exif_core_metadata.datetimeor::timestamp - interval '13400 second') OR time=(photos_exif_core_metadata.datetimeor::timestamp) - interval '13401 second' )
 --, MIN(gps_tracks.time), MIN(gps_tracks.time)
+
+
+
+SELECT photos.session_id, 
+photos."FileName", photos.session_photo_number, gps_tracks.fid, photos."DateTimeOriginal"as Clock_Camera, gps_tracks."time"as Clock_GPS, photos."GPSDateTime" as Clock_GPS_Camera, photos."GPSLatitude", photos."GPSLongitude" 
+FROM photos_exif_core_metadata photos, gps_tracks 
+WHERE gps_tracks."time"=photos."DateTimeOriginal" LIMIT 300;
+
+
+SELECT photos.session_id, 
+photos."FileName", photos.session_photo_number, gps_tracks.fid, photos."DateTimeOriginal"as Clock_Camera, gps_tracks."time"as Clock_GPS, photos."GPSDateTime" as Clock_GPS_Camera, photos."GPSLatitude", photos."GPSLongitude" 
+FROM photos_exif_core_metadata photos, gps_tracks 
+WHERE (gps_tracks."time"=(photos."DateTimeOriginal" - interval '260 second') OR gps_tracks.time=(photos."DateTimeOriginal" - interval '261 second'))  LIMIT 800;
+
+
+ 
+ 
