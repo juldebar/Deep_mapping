8b8c15d4bb78aa15ef869bcaf3ff0b786c45cb38
juldebar <juldebar@gmail.com>
2018-08-03 12:23:09 +0200

    test generic function to extract exif metadata from photos

---
+++ test/extract_exif_metadata_in_this_directory.R
@@ -0,0 +1,51 @@
+extract_exif_metadata_in_this_directory <- function(this_directory,template_df, mime_type = "*.JPG"){
+  setwd(this_directory)
+  
+  log <- paste("Adding references for photos in ", this_directory, "\n", sep=" ")
+  parent_directory <- gsub(dirname(dirname(dirname(this_directory))),"",dirname(dirname(this_directory)))
+  parent_directory <- gsub("/","",parent_directory)
+  
+  files <- list.files(pattern = mime_type ,recursive = TRUE)
+  exif_metadata <-template_df
+  exif_metadata <- read_exif(files)
+  exif_metadata$session_id = parent_directory
+  exif_metadata$session_photo_number <-c(1:nrow(exif_metadata))# @julien => A INCREMENTER ?
+  exif_metadata$relative_path = gsub(dirname(images_directory),"",this_directory)
+  
+  # # IF THERE IS NO GPS DATA WE ADD EXPECTED COLUMNS WITH DEFAULT VALUES NA
+  if(exists("exif_metadata$GPSLatitude")==FALSE){
+    exif_metadata$GPSVersionID <-NA
+    exif_metadata$GPSLatitudeRef <-NA
+    exif_metadata$GPSLongitudeRef <-NA
+    exif_metadata$GPSAltitudeRef <-NA
+    exif_metadata$GPSTimeStamp <-NA
+    exif_metadata$GPSMapDatum <-NA
+    exif_metadata$GPSDateStamp <-NA
+    exif_metadata$GPSAltitude <-NA
+    exif_metadata$GPSDateTime <-NA
+    exif_metadata$GPSLatitude <-NA
+    exif_metadata$GPSLongitude <-NA
+    exif_metadata$GPSPosition <-NA
+  }
+  # change default data types
+  exif_metadata$GPSDateTime = as.POSIXct(unlist(exif_metadata$GPSDateTime),"%Y:%m:%d %H:%M:%S", tz="UTC")
+  exif_metadata$DateTimeOriginal = as.POSIXct(unlist(exif_metadata$DateTimeOriginal),"%Y:%m:%d %H:%M:%S", tz="Indian/Mauritius")
+  exif_metadata$GPSLatitude = as.numeric(exif_metadata$GPSLatitude)
+  exif_metadata$GPSLongitude = as.numeric(exif_metadata$GPSLongitude)
+  exif_metadata$geometry_postgis <- NA
+  exif_metadata$geometry_postgis = as.numeric(unlist(exif_metadata$geometry_postgis))
+  exif_metadata$geometry_gps_correlate <- NA
+  exif_metadata$geometry_gps_correlate = as.numeric(unlist(exif_metadata$geometry_gps_correlate))
+  exif_metadata$geometry_native <- NA
+  exif_metadata$geometry_native = as.numeric(unlist(exif_metadata$geometry_native))
+  
+  return(exif_metadata)
+}
+
+codes_directory <-"/home/julien/Bureau/CODES/Deep_mapping/"
+image_directory <- "/media/julien/39160875-fe18-4080-aab7-c3c3150a630d/julien/go_pro_all/session_2018_01_01_kite_Le_Morne/DCIM/100GOPRO"
+template_df <- read.csv(paste0(codes_directory,"CSV/All_Exif_metadata_template.csv"),stringsAsFactors = FALSE)
+extract_exif_metadata_in_this_directory(image_directory,template_df, mime_type = "*.JPG.jpg")
+setwd("/tmp")
+write.csv(exif_metadata, "exif_metadata.csv",row.names = F)
+
