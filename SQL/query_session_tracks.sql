2f8110f0802e937cebf46326b74c7ba07e25df86
juldebar <juldebar@gmail.com>
2018-08-07 13:48:48 +0200

    Query which gives the spatial and temporal extent for each session stored in the database

--- SQL/query_session_tracks.sql
+++ SQL/query_session_tracks.sql
@@ -1,1 +1,15 @@
-SELECT session_id, array_agg(gps_tracks.fid) AS list_gps_points, array_agg(gps_tracks.time) AS time, min(gps_tracks.time) AS start_date,max(gps_tracks.time) AS end_date, ST_AsEWKT(ST_MakeLine(the_geom)) AS WKT, ST_MakeLine(the_geom) AS track from gps_tracks  where session_id='session_2018_03_31_kite_Le_Morne' GROUP by session_id;
+SELECT 
+session_id, 
+array_agg(gps_tracks.fid) AS list_gps_points, 
+array_agg(gps_tracks.time) AS time, 
+min(gps_tracks.time) AS start_date, 
+max(gps_tracks.time) AS end_date, 
+'start='::text || MIN(gps_tracks.time)::text || ';end='::text ||MAX(gps_tracks.time)::text AS temporal_coverage,
+ST_AsEWKT(ST_MakeLine(the_geom)) AS spatial_coverage, 
+ST_MakeLine(the_geom) AS track 
+FROM 
+ gps_tracks  
+WHERE 
+session_id='session_2018_03_31_kite_Le_Morne' 
+GROUP BY 
+session_id;
