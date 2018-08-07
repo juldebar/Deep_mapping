083ad3cff39884f47a9f39d4ec7526d70b7e09fb
juldebar <juldebar@gmail.com>
2018-08-07 06:10:16 +0400

    add condition on number of points in SQL query to infer location

--- SQL/interpolation_between_closest_GPS_POINTS.sql
+++ SQL/interpolation_between_closest_GPS_POINTS.sql
@@ -20,7 +20,7 @@ FROM (
 SELECT photos.session_id AS session_id, gps_tracks.fid AS GPS_trackpoint_id, photos."FileName", gps_tracks.the_geom
 FROM photos_exif_core_metadata photos, gps_tracks 
 WHERE (gps_tracks."time"=(photos."DateTimeOriginal" - interval '42 second') OR gps_tracks."time"=(photos."DateTimeOriginal" - interval '41 second'))
-AND gps_tracks.session_id=photos.session_id AND photos.session_id='session_2018_03_24_kite_Le_Morne' 
+AND gps_tracks.session_id=photos.session_id AND photos.session_id='session_2018_03_31_kite_Le_Morne' 
 -- LIMIT 1000 
 ) AS foo 
 GROUP BY session_id, "FileName" 
@@ -29,6 +29,14 @@ GROUP BY  session_id, list_gps_points, segment
 ORDER BY  session_id, list_gps_points
 ) AS final_query
 
-WHERE photos_in_segment > 1 AND photos_in_segment < 3 AND list_photos @> ARRAY[photos_exif_core_metadata."FileName"]
+WHERE photos_in_segment > 1 AND photos_in_segment < 3 
+-- AND list_photos @> ARRAY[photos_exif_core_metadata."FileName"]
 LIMIT 800
 ;
+
+
+
+
+
+
+
