ed7c04863310ddad943b46bfd1785ce12e7d0e2e
juldebar <juldebar@gmail.com>
2018-08-10 13:47:30 +0200

    new SQL query to get nearest gps points (in time) for a given picture

--- SQL/template_interpolation_between_closest_GPS_POINTS.sql
+++ SQL/template_interpolation_between_closest_GPS_POINTS.sql
@@ -12,22 +12,23 @@ FROM
     (SELECT session_id, array_agg("FileName") AS list_photos, array_length(array_agg("FileName"),1) AS photos_in_segment, list_gps_points, count_gps_points, segment
         FROM 
         (
-        SELECT 
-        row_number() OVER(ORDER BY session_id) AS OID, 
-        session_id, "FileName", 
-        array_agg(GPS_trackpoint_id) AS list_gps_points, 
-        array_length(array_agg(GPS_trackpoint_id),1) AS count_gps_points, 
-        ST_AsEWKT(ST_MakeLine(the_geom)) AS segment 
-        FROM 
-                      (
-                      SELECT photos.session_id AS session_id, gps_tracks.fid AS GPS_trackpoint_id, photos."FileName", gps_tracks.the_geom
-                      FROM photos_exif_core_metadata photos, gps_tracks 
-                      WHERE (gps_tracks."time"=(photos."DateTimeOriginal" - interval '42 second') OR gps_tracks."time"=(photos."DateTimeOriginal" - interval '41 second'))
-                      AND gps_tracks.session_id=photos.session_id AND photos.session_id='session_2018_03_24_kite_Le_Morne' 
-                      ORDER BY GPS_trackpoint_id
-                      ) AS foo 
-        GROUP BY session_id, "FileName" 
-        ORDER BY session_id, "FileName") AS the_query  
+		SELECT 
+		row_number() OVER(ORDER BY session_id) AS OID, 
+		session_id, "FileName", 
+		array_agg(GPS_trackpoint_id) AS list_gps_points, 
+		array_length(array_agg(GPS_trackpoint_id),1) AS count_gps_points, 
+		ST_AsEWKT(ST_MakeLine(the_geom)) AS segment 
+		FROM 
+		              (
+		              SELECT photos.session_id AS session_id, gps_tracks.fid AS GPS_trackpoint_id, photos."FileName", gps_tracks.the_geom
+		              FROM photos_exif_core_metadata photos, gps_tracks 
+		              WHERE (gps_tracks."time"=(photos."DateTimeOriginal" - interval '42 second') OR gps_tracks."time"=(photos."DateTimeOriginal" - interval '41 second'))
+		              AND gps_tracks.session_id=photos.session_id AND photos.session_id='session_2018_03_24_kite_Le_Morne' 
+		              ORDER BY GPS_trackpoint_id
+		              ) AS foo 
+		GROUP BY session_id, "FileName" 
+		ORDER BY session_id, "FileName"
+		) AS the_query  
         GROUP BY  session_id, list_gps_points, count_gps_points, segment
         ORDER BY  session_id, list_gps_points
         ) AS final_query
