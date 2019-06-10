f03599e39ea2240b97e143cad66c51d53fcdd136
juldebar <juldebar@gmail.com>
2019-06-10 11:12:36 +0400

    improve SQL queries

--- SQL/create_tables_GPS_tracks.sql
+++ SQL/create_tables_GPS_tracks.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS  "public"."gps_tracks" CASCADE;
 CREATE TABLE "public"."gps_tracks"(
     fid serial NOT NULL,
     session_id character varying(254),
+    "time" timestamp with time zone,
     latitude double precision,
     longitude double precision,
     altitude double precision,
-    "time" timestamp with time zone,
     heart_rate double precision,
     the_geom geometry(Point,4326),
     CONSTRAINT activities_pk PRIMARY KEY (fid,session_id)
