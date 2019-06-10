bf5c9a589303aa14e73206e45a678efe0bcaf83b
juldebar <juldebar@gmail.com>
2019-06-10 15:10:44 +0400

    improve automated extraction of metadata for each session

--- R/create_metadata_dataframe.R
+++ R/create_metadata_dataframe.R
@@ -5,39 +5,24 @@ library(trackeR)
 library(dplyr)
 library(exifr)
 library(data.table)
+codes_directory <-"/home/julien/Bureau/CODES/Deep_mapping/"
 
 ############################################################
 ################### Set directory #######################
 ############################################################
-con_Reef_database <- dbConnect(DRV, user=User, password=Password, dbname=Dbname, host=Host)
-template_query_create_view <- paste(readLines("/home/julien/Bureau/CODES/Deep_mapping/SQL/create_view_session_template.sql"), collapse=" ")
-query_metadata <- "SELECT * FROM metadata"
-get_metadata  <- dbGetQuery(con_Reef_database,query_metadata)
-number_row<-nrow(get_metadata)
-for (i in 1:number_row) {
-  query_create_view<- gsub("view_session_2018_03_31_kite_Le_Morne",get_metadata$related_view_name[i],template_query_create_view)
-  query_create_view<- gsub("session_2018_03_31_kite_Le_Morne",get_metadata$identifier[i],template_query_create_view)
-  create_view  <- dbGetQuery(con_Reef_database,query_create_view)
-}
-
-
-############################################################
-################### Set directory #######################
-############################################################
-working_directory <-  "/media/juldebar/ab29186c-4812-4fa3-bf4d-583f3f5ce311/julien/Deep_Mapping/data_deep_mapping/gopro1/checked"
+working_directory <-  "/media/julien/Deep_Mapping_4To/data_deep_mapping/2019/A"
 setwd(working_directory)
 
 sub_directories <- list.dirs(path=working_directory,full.names = TRUE,recursive = FALSE)
-# sub_directories <- list.dirs(path=working_directory,full.names = FALSE,recursive = FALSE)
 number_sub_directories <-length(sub_directories)
 
-metadata_pictures <- data.frame(session=character(), path=character(), file_name=character(), spatial_extent=character(), temporal_extent=character(), number_pictures=integer())
+metadata_pictures <- data.frame(session_id=character(), path=character(), gps_file_name=character(), spatial_extent=character(), temporal_extent=character(), Number_of_Pictures=integer())
 
 for (i in 1:number_sub_directories){
   this_directory <- sub_directories[i]
   setwd(this_directory)
-  name_session <- gsub(paste0(dirname(this_directory),"/"),"",this_directory)
-  tcx_file <- NULL
+  session_id <- gsub(paste0(dirname(this_directory),"/"),"",this_directory)
+  gps_file <- NULL
   temporal_extent <- NULL
   bounding_box <- NULL
   Number_of_Pictures <- NULL
@@ -48,7 +33,6 @@ for (i in 1:number_sub_directories){
   files <- NULL
   files <- list.files(path = paste(this_directory,"DCIM",sep="/"), pattern = "*.JPG",recursive = TRUE)
   if(length(files)>0){
-    
     Number_of_Pictures <- length(files)
     ############################################################
     ################### TEMPORAL COVERAGE ######################
@@ -72,31 +56,56 @@ for (i in 1:number_sub_directories){
   ############################################################
   ################### SPATIAL COVERAGE #######################
   ############################################################
-  tcx_file <- list.files(path=paste(this_directory,"GPS",sep="/"), pattern = "*.tcx",recursive = FALSE)
-  if(length(tcx_file)>0){
-    runDF <- NULL
-    #   # runDF <- readTCX(file=file, timezone = "GMT")
-    runDF <- readTCX(file=paste(this_directory,"GPS",tcx_file,sep="/"))
-    select_columns = subset(runDF, select = c(latitude,longitude,altitude,time,heart.rate))
-    GPS_tracks_values <- NULL
-    GPS_tracks_values = rename(select_columns, latitude=latitude,longitude=longitude, altitude=altitude, heart_rate=heart.rate, time=time)
-    names(GPS_tracks_values)
-    xmin <- min(GPS_tracks_values$longitude)
-    xmax <- max(GPS_tracks_values$longitude)
-    ymin <- min(GPS_tracks_values$latitude)
-    ymax <- max(GPS_tracks_values$latitude)
-    bounding_box <- WKT <- paste("POLYGON((",xmin,ymin,",",xmin,ymax,",",xmax,ymax,",",xmax,ymin,",",xmin,ymin,"))",sep=" ")
+  file_type<-"TCX" #  "GPX"  "TCX" "RTK"
+  dataframe_gps_files <- return_dataframe_gps_files(this_directory,type=file_type)
+  if(is.null(dataframe_gps_files)){
+    file_type<-"GPX"
+    dataframe_gps_files <- return_dataframe_gps_files(this_directory,type=file_type)
+    }
+  number_row<-nrow(dataframe_gps_files)
+  if(number_row>0){
+    for (t in 1:number_row){
+      gps_file <- paste(dataframe_gps_files$path[t],dataframe_gps_files$file_name[t],sep="/")
+      dataframe_gps_file <-NULL
+      dataframe_gps_file <- return_dataframe_gps_file(wd=codes_directory, gps_file=gps_file, type=file_type, session_id=session_id)
+#       name(dataframe_gps_file)
+      xmin <- min(dataframe_gps_file$longitude)
+      xmax <- max(dataframe_gps_file$longitude)
+      ymin <- min(dataframe_gps_file$latitude)
+      ymax <- max(dataframe_gps_file$latitude)
+      bounding_box <- WKT <- paste("POLYGON((",xmin,ymin,",",xmin,ymax,",",xmax,ymax,",",xmax,ymin,",",xmin,ymin,"))",sep=" ")
+    }
   }else{
-    tcx_file <- "No GPS file"
+    (cat("No GPS file when looking for TCX or GPX or RTK files"))
+    gps_file <- "No GPS file"
     bounding_box <- "No GPS file"
-  }
+    }
+  
   ############################################################
   ################### CREATE DATAFRAME #######################
   ############################################################
-  newRow <- c(name_session,this_directory,tcx_file,bounding_box, temporal_extent, Number_of_Pictures)
+  newRow <- data.frame(session_id=session_id,path=this_directory,gps_file_name=gps_file,spatial_extent=bounding_box, temporal_extent=temporal_extent, Number_of_Pictures=Number_of_Pictures)
   metadata_pictures <- rbind(metadata_pictures,newRow)
 }
+
+
 head(metadata_pictures)
 setwd(working_directory)
-write.csv(metadata_pictures, file = "metadata_sessions.csv")
-sum(metadata_pictures$number_pictures)
+write.csv(metadata_pictures,file = "metadata_sessions.csv",row.names = F)
+nrow(metadata_pictures)
+sum(metadata_pictures$Number_of_Pictures)
+
+
+########################################################################################################################
+################### Create a view for each session with the track of the survey as a polyline #######################
+########################################################################################################################
+con_Reef_database <- dbConnect(DRV, user=User, password=Password, dbname=Dbname, host=Host)
+template_query_create_view <- paste(readLines("/home/julien/Bureau/CODES/Deep_mapping/SQL/create_view_session_template.sql"), collapse=" ")
+query_metadata <- "SELECT * FROM metadata"
+get_metadata  <- dbGetQuery(con_Reef_database,query_metadata)
+number_row<-nrow(get_metadata)
+for (i in 1:number_row) {
+  query_create_view<- gsub("view_session_2018_03_31_kite_Le_Morne",get_metadata$related_view_name[i],template_query_create_view)
+  query_create_view<- gsub("session_2018_03_31_kite_Le_Morne",get_metadata$identifier[i],template_query_create_view)
+  create_view  <- dbGetQuery(con_Reef_database,query_create_view)
+}
