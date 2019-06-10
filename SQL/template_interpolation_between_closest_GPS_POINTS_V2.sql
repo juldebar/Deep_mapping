2ddb6298224a13222d379606e0c6aaf80637d088
juldebar <juldebar@gmail.com>
2019-06-10 11:13:20 +0400

    improve SQL queries

--- SQL/template_interpolation_between_closest_GPS_POINTS_V2.sql
+++ SQL/template_interpolation_between_closest_GPS_POINTS_V2.sql
@@ -2,7 +2,7 @@ SELECT
 	photos_exif_core_metadata.ogc_fid, 
 	photos_exif_core_metadata.session_id, 
 	photos_exif_core_metadata.session_photo_number, 
-	CONCAT(photos_exif_core_metadata."relative_path"||'/'||photos_exif_core_metadata."FileName") AS file_path, 
+	CONCAT(photos_exif_core_metadata."relative_path"||'/'||photos_exif_core_metadata."FileName") AS photo_relative_file_path, 
 	photos_in_segments.list_photos, 
 	unnest(array_positions(list_photos, photos_exif_core_metadata."FileName")) AS cell_number, 
 	photos_in_segments.list_time_photos, 
@@ -23,13 +23,29 @@ FROM
 		segments 
 		FROM 
 			(
-				SELECT GPS1.fid AS GPS1_fid, GPS2.fid AS GPS2_fid, photos."FileName", photos."DateTimeOriginal", GPS1.session_id, GPS1."time" AS time_first_point, GPS2."time" AS time_second_point, (GPS2."time"-GPS1."time") AS diff, ST_AsEWKT(ST_MakeLine(GPS1.the_geom,GPS2.the_geom)) segments  
-				FROM gps_tracks as GPS1, gps_tracks as GPS2, photos_exif_core_metadata AS photos 
-				WHERE  GPS2.fid = GPS1.fid +1
-				 AND GPS1.session_id=GPS2.session_id AND GPS1.session_id=photos.session_id AND GPS1.session_id='session_2018_08_25_Zanzibar_Snorkelling' 
-				 AND photos."DateTimeOriginal" < GPS2."time" AND  photos."DateTimeOriginal" >= GPS1."time" 
+			SELECT 
+				GPS1.fid AS GPS1_fid, 
+				GPS2.fid AS GPS2_fid, 
+				photos."FileName", 
+				photos."DateTimeOriginal", 
+				GPS1.session_id, 
+				GPS1."time" AS time_first_point, 
+				GPS2."time" AS time_second_point, 
+				(GPS2."time"-GPS1."time") AS diff, 
+				ST_AsEWKT(ST_MakeLine(GPS1.the_geom,GPS2.the_geom)) segments  
+			FROM 
+				gps_tracks as GPS1, 
+				gps_tracks as GPS2, 
+				photos_exif_core_metadata AS photos 
+			WHERE  
+				GPS2.fid = GPS1.fid +1
+				AND GPS1.session_id=GPS2.session_id 
+				AND GPS1.session_id=photos.session_id 
+				AND GPS1.session_id='session_2018_08_25_Zanzibar_Snorkelling' 
+				AND photos."DateTimeOriginal" < GPS2."time" 
+				AND photos."DateTimeOriginal" >= GPS1."time" 
 
-				ORDER BY GPS1.session_id, GPS1.fid ASC
+			ORDER BY GPS1.session_id, GPS1.fid ASC
 			) AS foo
 		GROUP BY session_id, GPS1_fid, segments
 		ORDER BY ogc_fid
@@ -40,38 +56,22 @@ WHERE
 	AND photos_exif_core_metadata.session_id=photos_in_segments.session_id 
 	AND photos_exif_core_metadata.session_id='session_2018_08_25_Zanzibar_Snorkelling'
 
+ORDER BY photos_exif_core_metadata."FileName"
+
+
+-- CHECK ST_LineInterpolatePoints AND ST_LineLocatePoint
+-- ORDER BY count_photos DESC
+--  AND GPS1."time" AT TIME ZONE 'UTC' = (GPS2."time" AT TIME ZONE 'UTC' - interval '1 second')
+-- GROUP BY diff(ogc_fid) < 1
+
+-- ET
+-- Pour chaque segment créé:
+-- 	- select toutes les photos dont la date est inférieure à celle du deuxième point du segment et supérieure à celle du premier point.
 
 
 
 
-	
-
-
-CHECK ST_LineInterpolatePoints AND ST_LineLocatePoint
-
-
-
-
-
-ORDER BY count_photos DESC
-
-
-
-
- AND GPS1."time" AT TIME ZONE 'UTC' = (GPS2."time" AT TIME ZONE 'UTC' - interval '1 second')
-GROUP BY diff(ogc_fid) < 1
---session_id='session_2018_03_24_kite_Le_Morne' AND
-
-ET
-Pour chaque segment créé:
-	- select toutes les photos dont la date est inférieure à celle du deuxième point du segment et supérieure à celle du premier point.
-
-
-
-
-
-
-250000 => ferry
-100000 => hotel
-Resto / pot => 55
-100€ Pascaline (50+50 sylvain)
+-- 250000 => ferry
+-- 100000 => hotel
+-- Resto / pot => 55
+-- 100€ Pascaline (50+50 sylvain)
