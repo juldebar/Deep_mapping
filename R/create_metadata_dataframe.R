2c873a7d22431dce0e4a909902f6f3eadb57d623
juldebar <juldebar@gmail.com>
2019-06-19 15:42:51 +0400

    improve metadata extraction

--- R/create_metadata_dataframe.R
+++ R/create_metadata_dataframe.R
@@ -5,17 +5,25 @@
 ############################################################
 ################### Packages #######################
 ############################################################
+rm(list=ls())
+
 library(trackeR)
 library(dplyr)
 library(exifr)
 library(data.table)
+library(geoflow)
+library(sf)
+library(pdftools)
+library(RgoogleMaps)
+library(rosm)
+
+
+################### Set directories #######################
 codes_directory <-"/home/julien/Bureau/CODES/Deep_mapping/"
 setwd(codes_directory)
 source(paste0(codes_directory,"R/functions.R"))
-############################################################
-################### Set directory #######################
-############################################################
-working_directory <-  "/media/julien/Deep_Mapping_4To/data_deep_mapping/2019/A"
+# working_directory <-  "/media/julien/Deep_Mapping_4To/data_deep_mapping/2019/A/pending/"
+# working_directory <-  "/media/julien/Deep_Mapping_4To/data_deep_mapping/2019/A"
 # working_directory <- "/media/julien/Deep_Mapping_4To/data_deep_mapping/2018/A"
 working_directory <-"/media/julien/Deep_Mapping_4To/data_deep_mapping/2019/A/done"
 setwd(working_directory)
@@ -36,23 +44,25 @@ for (i in 1:number_sub_directories){
   Number_of_Pictures <- NULL
   relation <-"parent:my-parent-metadata-identifier@http://catalogue/my-parent-metadata-identifier;
 http:website@http://somelink/website"
-  rights <-"use:terms1;
-use:citation1;
-use:disclaimer1;
-useConstraint:copyright;
-useConstraint:license;
-accessConstraint:copyright;
-otherConstraint:web use;"
-  provenance <-"statement:My data management workflow;
-process:
-rationale1[description1],
-rationale2[description2],
-rationale3[description3]
-processor:
-emmanuel.blondel1@gmail.com,
-julien.barde@ird.fr,
-wilfried.heintz@inra.fr"
-  data <-"identifier:layer1\nsource:D://geoflow-sandbox/shapefile1.zip;\nsourceName:shapefile1;\ntype:shp;\nupload:true;"
+  rights <-""
+#   rights <-"use:terms1;
+#   use:citation1;
+# use:disclaimer1;
+# useConstraint:copyright;
+# useConstraint:license;
+# accessConstraint:copyright;
+# otherConstraint:web use;"
+  provenance <-""
+#   provenance <-"statement:My data management workflow;
+# process:
+# rationale1[description1],
+# rationale2[description2],
+# rationale3[description3]
+# processor:
+# emmanuel.blondel1@gmail.com,
+# julien.barde@ird.fr,
+# wilfried.heintz@inra.fr"
+  
   GPS_timestamp <- NULL
   Photo_GPS_timestamp <- NULL
   
@@ -69,7 +79,9 @@ wilfried.heintz@inra.fr"
     # geoflow entities data structure => 2007-03-01T13:00:00Z/2008-05-11T15:30:00Z
     first_picture_metadata <- read_exif(paste(this_directory,"DCIM", files[1],sep="/"))
     start_date<- as.POSIXct(first_picture_metadata$DateTimeOriginal, "%Y:%m:%d %H:%M:%OS", tz="UTC")
+    start_date<-paste0(gsub(" ","T",start_date),"Z")
     end_date<- as.POSIXct(read_exif(paste(this_directory,"DCIM",files[Number_of_Pictures],sep="/"))$DateTimeOriginal, "%Y:%m:%d %H:%M:%OS", tz="UTC")
+    end_date<-paste0(gsub(" ","T",end_date),"Z")
     # temporal_extent <- paste0("start=", start_date,";end=",end_date)
     temporal_extent <- paste0(start_date,"/",end_date)
     ############################################################
@@ -87,7 +99,7 @@ wilfried.heintz@inra.fr"
   ############################################################
   ################### SPATIAL COVERAGE #######################
   ############################################################
-  file_type<-"TCX" #  "GPX"  "TCX" "RTK"
+  file_type<-"TCX" #  "GPX"  "TCX" "RTK" "videos" "aerial"
   dataframe_gps_files <- return_dataframe_gps_files(this_directory,type=file_type)
   if(is.null(dataframe_gps_files)){
     file_type<-"GPX"
@@ -99,12 +111,44 @@ wilfried.heintz@inra.fr"
       gps_file <- paste(dataframe_gps_files$path[t],dataframe_gps_files$file_name[t],sep="/")
       dataframe_gps_file <-NULL
       dataframe_gps_file <- return_dataframe_gps_file(wd=codes_directory, gps_file=gps_file, type=file_type, session_id=session_id)
-#       name(dataframe_gps_file)
+#       head(dataframe_gps_file)
       xmin <- min(dataframe_gps_file$longitude)
       xmax <- max(dataframe_gps_file$longitude)
       ymin <- min(dataframe_gps_file$latitude)
       ymax <- max(dataframe_gps_file$latitude)
-      spatial_extent <- WKT <- paste("POLYGON((",xmin,ymin,",",xmin,ymax,",",xmax,ymax,",",xmax,ymin,",",xmin,ymin,"))",sep=" ")
+      spatial_extent <- WKT <- paste("SRID=4326;POLYGON((",xmin,ymin,",",xmin,ymax,",",xmax,ymax,",",xmax,ymin,",",xmin,ymin,"))",sep=" ")
+      
+#       jpeg(paste0(session_id,".jpg"))
+#       bmaps.plot(bbox, type = "Aerial",res=300,zoomin=-1,stoponlargerequest=FALSE)
+#       # prettymap(bmaps.plot(bbox, type = "Aerial",zoomin=-1,stoponlargerequest=FALSE),res=300, scale.style="ticks", scale.tick.cex=0.5)
+#       osm.points(dataframe_gps_file$longitude,dataframe_gps_file$latitude, col="yellow",pch=18, cex=0.5)
+#       dev.off()
+
+      this_wd <- getwd()
+      setwd(as.character(dataframe_gps_files$path[t]))
+      
+      gps_points <- st_as_sf(dataframe_gps_file, coords = c("longitude", "latitude"),crs = 4326)
+      bbox <- makebbox(ymax,xmax,ymin,xmin)
+      bbox <- makebbox(ymax+0.03,xmax+0.03,ymin-0.03,xmin-0.03)
+      bbox
+      
+      pdf(paste0(session_id,".pdf"))
+      bmaps.plot(bbox, type = "Aerial",res=600,zoomin=-1,stoponlargerequest=FALSE)
+      # prettymap(bmaps.plot(bbox, type = "Aerial",zoomin=-1,stoponlargerequest=FALSE),res=300, scale.style="ticks", scale.tick.cex=0.5)
+      osm.points(dataframe_gps_file$longitude,dataframe_gps_file$latitude, col="yellow",pch=18, cex=0.5)
+      dev.off()
+      
+      # https://cran.r-project.org/web/packages/pdftools/pdftools.pdf
+      pdf_convert(paste0(session_id,".pdf"),format = "jpeg",dpi = 600)
+      
+      file_name <- gsub("\\.","_",dataframe_gps_files$file_name[t])
+      # file_name <- gsub("x","x.csv",file_name)
+      shape_file <- write_shp_from_csv(file_name)
+      # qgs_template <- "/home/julien/Bureau/CODES/Deep_mapping/template/qgis_project_csv.qgs"
+      qgs_template <- "/home/julien/Bureau/CODES/Deep_mapping/template/qgis_project_shapefile_new.qgs"
+      write_qgis_project(session_id, qgs_template,shape_file,xmin,xmax,ymin,ymax)
+      setwd(this_wd)
+
     }
   }else{
     (cat("No GPS file when looking for TCX or GPX or RTK files"))
@@ -118,50 +162,65 @@ wilfried.heintz@inra.fr"
   close(con)
   
   first_line <- sub('.*session', 'session', first_line)
-  photo_calibration_metadata <- read_exif(paste(gsub(session_id,"",this_directory), sub(' => .*', '', first_line),sep="/"))
+  photo_calibration_metadata <- read_exif(paste(gsub(session_id,"",this_directory), sub(' => .*', '', first_line),sep=""))
   Photo_GPS_timestamp<- as.POSIXct(photo_calibration_metadata$DateTimeOriginal, "%Y:%m:%d %H:%M:%OS", tz="UTC")
   
   GPS_timestamp <- sub('.* => ', '', first_line)
   GPS_timestamp <- sub('secs\").*', 'secs\")', GPS_timestamp)
-  Photo_GPS_timestamp <- first_line
   offset <- GPS_timestamp
   
+  
+  title <- gsub("_"," ",session_id)
+  title <- gsub("201"," of the 201",title)
+  description <- paste0("abstract:This dataset is made of ",Number_of_Pictures," pictures which have been collected during the ", title)
+  
+  # data <-paste0("identifier:",session_id,";\nsource:",gps_file,";\nsourceName:",session_id,";\ntype:dataset;\nupload:true;")
+  # data <-paste0("source:file:///tmp/dessin.pdf;\nsourceName:",session_id,";\ntype:other;\nupload:true;")
+  data <-paste0("source:http://www.fao.org/3/i7805en/I7805EN.pdf;\nsourceName:",session_id,";\ntype:other")
   
   ############################################################
   ################### CREATE DATAFRAME #######################
   ############################################################
   # metadata_sessions <- data.frame(Identifier=character(), Date=character(), path=character(), gps_file_name=character(), SpatialCoverage=character(), TemporalCoverage=character(),Relation=character(), Rights=character(), Provenance=character(), Data=character(), Number_of_Pictures=integer(), GPS_timestamp=character(),  Photo_GPS_timestamp=character())
-  newRow <- data.frame(Identifier=session_id,Date=date,path=this_directory,gps_file_name=gps_file,SpatialCoverage=spatial_extent, TemporalCoverage=temporal_extent, Relation=relation,Rights=data, Provenance=data, Data=data, Number_of_Pictures=Number_of_Pictures, GPS_timestamp=GPS_timestamp, Photo_GPS_timestamp=Photo_GPS_timestamp)
+  newRow <- data.frame(Identifier=paste0("id:",session_id,";"),Title=title,	Description=description, Date=date,path=this_directory,gps_file_name=gps_file,SpatialCoverage=spatial_extent, TemporalCoverage=temporal_extent, Relation=relation,Rights=rights, Provenance=provenance, Data=data, Number_of_Pictures=Number_of_Pictures, GPS_timestamp=GPS_timestamp, Photo_GPS_timestamp=Photo_GPS_timestamp)
   metadata_sessions <- rbind(metadata_sessions,newRow)
 }
 
-metadata_sessions$Title <- "Session Title"
-metadata_sessions$Description <- "Session Summary"
-metadata_sessions$Subject <- "GENERAL=Mauritius, coral reef, photos, deep learning, kite surfing, coral reef habitats"
+metadata_sessions$Subject <- "GENERAL:Mauritius,coral reef,photos,deep learning,kite surfing,coral reef habitats;"
 metadata_sessions$Creator <- "owner:emmanuel.blondel1@gmail.com;\npointOfContact:julien.barde@ird.fr,wilfried.heintz@inra.fr;"
 metadata_sessions$Type <- "dataset"
 metadata_sessions$Language <- "eng"
 
 names(metadata_sessions)
 head(metadata_sessions)
-metadata_sessions <- metadata_sessions[,c(1,14,15,16,17,2,16,17,5,6,7,8,9,10,3,4,11,12,13)]
+# Identifier	Title	Description	Subject	Creator	Date	Type	Language	SpatialCoverage	TemporalCoverage	Relation	Rights	Provenance	Data	path	gps_file_name	Number_of_Pictures	GPS_timestamp	Photo_GPS_timestamp
+# Identifier	Title	Description	Subject	Creator	Date	Type	Language	SpatialCoverage	TemporalCoverage	Relation	Rights	Provenance	Data	Node_ID	Vid	DOI	Reference	IRD_reference	IOTC_title	Meeting	Meeting_session	Meeting_year	Authors	All_taxonomy_terms	File	Path	Abstract		
+metadata_sessions <- metadata_sessions[,c(1,2,3,16,17,4,18,19,7,8,9,10,11,12,5,6,13,14,15)]
 
 setwd(working_directory)
 write.csv(metadata_sessions,file = "metadata_sessions.csv",row.names = F)
 nrow(metadata_sessions)
 sum(metadata_sessions$Number_of_Pictures)
 
-
+########################################################################################################################
+################### Run geoflow #######################
+########################################################################################################################
+setwd(paste0(codes_directory,"R/geoflow"))
+executeWorkflow(file = "worflow_zenodo_step1.json")
 ########################################################################################################################
 ################### Create a view for each session with the track of the survey as a polyline #######################
 ########################################################################################################################
-con_Reef_database <- dbConnect(DRV, user=User, password=Password, dbname=Dbname, host=Host)
-template_query_create_view <- paste(readLines("/home/julien/Bureau/CODES/Deep_mapping/SQL/create_view_session_template.sql"), collapse=" ")
-query_metadata <- "SELECT * FROM metadata"
-get_metadata  <- dbGetQuery(con_Reef_database,query_metadata)
-number_row<-nrow(get_metadata)
-for (i in 1:number_row) {
-  query_create_view<- gsub("view_session_2018_03_31_kite_Le_Morne",get_metadata$related_view_name[i],template_query_create_view)
-  query_create_view<- gsub("session_2018_03_31_kite_Le_Morne",get_metadata$identifier[i],template_query_create_view)
-  create_view  <- dbGetQuery(con_Reef_database,query_create_view)
-}
+# con_Reef_database <- dbConnect(DRV, user=User, password=Password, dbname=Dbname, host=Host)
+# template_query_create_view <- paste(readLines("/home/julien/Bureau/CODES/Deep_mapping/SQL/create_view_session_template.sql"), collapse=" ")
+# query_metadata <- "SELECT * FROM metadata"
+# get_metadata  <- dbGetQuery(con_Reef_database,query_metadata)
+# number_row<-nrow(get_metadata)
+# for (i in 1:number_row) {
+#   query_create_view<- gsub("view_session_2018_03_31_kite_Le_Morne",get_metadata$related_view_name[i],template_query_create_view)
+#   query_create_view<- gsub("session_2018_03_31_kite_Le_Morne",get_metadata$identifier[i],template_query_create_view)
+#   create_view  <- dbGetQuery(con_Reef_database,query_create_view)
+# }
+
+# http://pierreroudier.github.io/teaching/20170626-Pedometrics/20170626-soil-data.html
+
+
