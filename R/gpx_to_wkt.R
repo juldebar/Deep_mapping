3523c0671a1229e04c00f908e15977999d69edeb
Barde <juldebar@ordibibi3>
2020-02-03 18:30:45 +0400

    update to fit new database model and other types of images: drone...

--- R/gpx_to_wkt.R
+++ R/gpx_to_wkt.R
@@ -8,9 +8,8 @@ gpx_to_wkt <- function(gps_file, dTolerance = 0.00005){
   class(spatial_data)
   spatial_data <- st_read(gps_file,layer = "tracks")
   session_track <- st_as_text(spatial_data$geometry)
-  simple_session_track <- st_as_text(st_simplify(spatial_data$geometry, dTolerance = dTolerance))
-  simple_session_track
-  wkt <- simple_session_track
+  # session_track <- st_as_text(st_simplify(spatial_data$geometry, dTolerance = dTolerance))
+  wkt <- session_track
   return(wkt)
 }
 
@@ -22,10 +21,9 @@ tcx_to_wkt <- function(gps_file,dTolerance=0.00005){
   dataframe_gps_file <- readTCX(file=gps_file, timezone = "UTC")
   dataframe_gps_file
   gps_points <- st_as_sf(dataframe_gps_file, coords = c("longitude", "latitude"),crs = 4326)
-  session_track <- gps_points %>% st_coordinates() %>% st_linestring()
-  simple_session_track <- session_track %>% st_simplify(dTolerance = dTolerance)  %>% st_as_text()
-  simple_session_track
-  wkt <- simple_session_track
+  session_track <- gps_points %>% st_coordinates() %>% st_linestring()  %>% st_as_text()
+  # simple_session_track <- session_track %>% st_simplify(dTolerance = dTolerance)  %>% st_as_text()
+  wkt <- session_track
   
   # session_track <- st_as_text(gps_points$geometry %>% st_cast("LINESTRING"))
   # simple_session_track <- st_as_text(st_simplify(session_track, dTolerance = 0.00005))
@@ -33,6 +31,6 @@ tcx_to_wkt <- function(gps_file,dTolerance=0.00005){
   return(wkt)
 }
 
-# gps_file <-"/home/juldebar/Téléchargements/22445995855.tcx"
+# gps_file <-"/home/juldebar/Téléchargements/activity_4057583961.tcx"
 # wkt <- tcx_to_wkt(gps_file,dTolerance = 0.00005)
 # wkt
