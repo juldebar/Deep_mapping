721ef497c69be32d855099a542fa004d0ea2eeeb
Barde <juldebar@ordibibi3>
2020-02-14 17:36:55 +0400

    updates in database model and related functions

--- SQL/add_spatial_column.sql
+++ SQL/add_spatial_column.sql
@@ -1,3 +1,5 @@
 UPDATE gps_tracks SET the_geom = ST_SetSRID(st_makepoint(longitude, latitude),4326);
+DROP INDEX IF EXISTS gps_tracks_geom_idx;
+CREATE INDEX gps_tracks_geom_idx ON "gps_tracks" USING GIST (the_geom);
 -- UPDATE gps_tracks SET the_geom = ST_GeomFromText('POINT ('::text || longitude	::text || ' '::text ||latitude::text || ')'::text, 4326) "
 --	SELECT AddGeometryColumn ('public','gps_tracks','the_geom',4326,'POINT',2);
