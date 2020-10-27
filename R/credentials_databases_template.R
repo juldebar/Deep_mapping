440ea8c6fe954f966b17ec9f7fd1da6095f3050c
Barde <juldebar@ordibibi3>
2020-10-27 15:01:46 +0100

    template file for credentials and configuration

---
+++ R/credentials_databases_template.R
@@ -0,0 +1,20 @@
+#######################
+##Database Identifiers:
+Dbname ="Reef_database"
+User="Reef_admin"
+Password="my_password"
+Host="reef-db.d4science.org"
+DRV="PostgreSQL"
+#######################
+#######################
+##Database Identifiers:
+Dbname ="Reef_database"
+User="postgres"
+Password="my_password"
+Host="localhost"
+DRV="PostgreSQL"
+#######################
+codes_directory <-"~/Bureau/CODES/Deep_mapping/"
+setwd(codes_directory)
+images_directory <- "/media/juldebar/c7e2c225-7d13-4f42-a08e-cdf9d1a8d6ac/Deep_Mapping/new"
+source(paste0(codes_directory,"R/credentials_databases.R"))
