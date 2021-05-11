bbce2b1a4d9f2d23b897bee423883e4545db7889
juldebar <juldebar@gmail.com>
2021-05-11 16:48:14 +0200

    align column names  with Darwin Core

--- SQL/select_pictures_by_area.sql
+++ SQL/select_pictures_by_area.sql
@@ -2,12 +2,12 @@ SELECT
 	photos.photo_id,
 	photos.session_id,	
 	photos."FileName",
-	ST_X(photos."geometry_postgis") AS decimalLongitude,
-	ST_Y(photos."geometry_postgis") AS decimalLatitude,
-	photos."DateTimeOriginal" AS eventDate,
+	ST_X(photos."geometry_postgis") AS "decimalLongitude"",
+	ST_Y(photos."geometry_postgis") AS "decimalLatitude"",
+	photos."DateTimeOriginal" AS "eventDate"",
 	CONCAT(photos.relative_path||'/'||photos."FileName") AS photo_path,
 	CONCAT(photos.session_id||'_'||photos."FileName") AS photo_name,
-	CONCAT('http://162.38.140.205/Deep_mapping/backup/validated'||photos.relative_path||'/'||photos."FileName") AS URL,
+	CONCAT('http://162.38.140.205/Deep_mapping/backup/validated'||photos.relative_path||'/'||photos."FileName") AS "URL",
 	annotation.photo_id AS map_photo_id
 
 FROM  public."photos_exif_core_metadata" AS photos LEFT JOIN annotation on annotation.photo_id=photos.photo_id 
