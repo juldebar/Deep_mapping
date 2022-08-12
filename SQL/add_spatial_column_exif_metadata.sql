80fcfd58abb34f4df65b023b6abac31ed009f058
juldebar <juldebar@gmail.com>
2022-08-12 19:10:12 +0200

    update SQL query

--- SQL/add_spatial_column_exif_metadata.sql
+++ SQL/add_spatial_column_exif_metadata.sql
@@ -1,3 +1,3 @@
-UPDATE photos_exif_core_metadata SET geometry_native = ST_SetSRID(st_makepoint("GPSLongitude", "GPSLatitude"),4326);
-DROP INDEX IF EXISTS photos_exif_core_metadata_geometry_native_geom_idx;
-CREATE INDEX photos_exif_core_metadata_geometry_native_geom_idx ON "photos_exif_core_metadata" USING GIST (geometry_native);
+UPDATE photos_exif_core_metadata SET geometry_native = ST_SetSRID(st_makepoint("GPSLongitude", "GPSLatitude"),4326) WHERE session_id LIKE 'session%';;
+--DROP INDEX IF EXISTS photos_exif_core_metadata_geometry_native_geom_idx;
+--CREATE INDEX photos_exif_core_metadata_geometry_native_geom_idx ON "photos_exif_core_metadata" USING GIST (geometry_native);
