3253e01c0c36e4f6a6249883f3b420a72c5410d1
juldebar <juldebar@gmail.com>
2023-07-18 16:40:32 +0200

    update database creation statements

--- SQL/create_Reef_database.sql
+++ SQL/create_Reef_database.sql
@@ -2,10 +2,10 @@ CREATE EXTENSION postgis;
 ALTER DATABASE "Reef_database" SET TimeZone = 'UTC';
 --DROP DATABASE "Reef_database";
 --CREATE DATABASE "Reef_database"
-  WITH OWNER = "Reef_admin"
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'fr_FR.UTF-8'
-       LC_CTYPE = 'fr_FR.UTF-8'
-       CONNECTION LIMIT = -1;
+--  WITH OWNER = "Reef_admin"
+--       ENCODING = 'UTF8'
+--       TABLESPACE = pg_default
+--      LC_COLLATE = 'fr_FR.UTF-8'
+--     LC_CTYPE = 'fr_FR.UTF-8'
+--     CONNECTION LIMIT = -1;
 
